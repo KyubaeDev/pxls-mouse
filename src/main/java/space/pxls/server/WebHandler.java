b23320363f62deb9bd9bb03c90785d51f107f405
Vanilla Black <vanilla@f66.dev>
2024-09-11 15:00:50 -0400

    Add setStack endpoint

--- src/main/java/space/pxls/server/WebHandler.java
+++ src/main/java/space/pxls/server/WebHandler.java
@@ -1618,6 +1618,37 @@ public class WebHandler {
         }
     }
 
+    public void setStacked(HttpServerExchange exchange) {
+        FormData data = exchange.getAttachment(FormDataParser.FORM_DATA);
+
+        try {
+            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+
+            String username = data.getFirst("user").getValue();
+            int count = Integer.parseInt(data.getFirst("count").getValue());
+
+            if (count < 0) {
+                exchange.setStatusCode(StatusCodes.BAD_REQUEST);
+                return;
+            }
+
+            User user = App.getUserManager().getByName(username);
+            if (user == null) {
+                exchange.setStatusCode(StatusCodes.NOT_FOUND);
+                return;
+            }
+
+            user.setStacked(count);
+
+            JSONObject json = new JSONObject();
+            json.put("status", "success");
+
+            exchange.getResponseSender().send(json.toString(4));
+        } catch (NullPointerException | NumberFormatException ex) {
+            exchange.setStatusCode(StatusCodes.BAD_REQUEST);
+        }
+    }
+
     public void profile(HttpServerExchange exchange) {
         exchange.getResponseHeaders()
                 .put(Headers.CONTENT_TYPE, "application/json");
